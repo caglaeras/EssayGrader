0ad6c1b1912f8a4ddd6d9455804ad7e4b30c438a
Çağla Eraslan <caglaeraslan@gmail.com>
2026-04-05 22:40:16 +0300

    Initialize Vite web app and configure GitHub Pages deployment

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import EssayGrader from './essay-grader'
+import './App.css' // You can leave this or remove if unused, but we'll include it to not break it if it has global resets
+
+function App() {
+  return (
+    <>
+      <EssayGrader />
+    </>
+  )
+}
+
+export default App
